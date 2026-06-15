268458a0254035a85ebca1730c423c2d804efdac
Alice Gon Enr <estrella_3042250020@utd.edu.mx>
2026-06-14 19:52:18 -0600

    Add files via upload

---
+++ P2/listas.py
@@ -0,0 +1,26 @@
+#Ejemplo 1 Crear una lista de numeros e imprimir el contenido
+print("\033c")
+numero=[23,45,23,33,25,100,-100]
+print(numero)
+
+lista="["
+for i in numero:
+    lista+=str(i)+","
+lista=lista[:-1]+"]"
+print(lista)
+
+#Ejemplo 2 Crear una lista de palabras y posteriormente buscar la coincidencia de una palabra 
+
+
+
+#2DA FORMA
+ 
+
+#3er FORMA
+
+#Ejemplo 3 Añadir elementos a la lista
+
+  
+
+#Ejemplo 4 Crear una lista multidimensional que permita almacenar el nombre y telefono de una agenda
+
